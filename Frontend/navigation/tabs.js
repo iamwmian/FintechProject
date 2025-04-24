a60dc294573076781a7f305689d5dea7ce78193b
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-04-24 13:48:03 -0500

    removed unnecessary imports.

--- Frontend/navigation/tabs.js
+++ Frontend/navigation/tabs.js
@@ -1,12 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Home/HomeScreen';
-import QrCodeScreen from '../screens/qr-code-screen';
 import { Ionicons } from '@expo/vector-icons';
-import LoginScreen from '../screens/SignIn/LoginScreen';
-import RegisterScreen from '../screens/Register/RegisterScreen';
 import SettingsScreen from '../screens/Settings/SettingsScreen';
-import { AuthenticationStack } from './stack';
-import ProfileDetailScreen from '../screens/profiles/profiles-detail-screen';
 import { COLORS } from '../common/theme';
 import { StyleSheet } from 'react-native';
 
