de140508b1eb70aaf6518582dd6ab90ecd83b05b
Chigoziri <ChigoziriOnwenu@gmail.com>
2025-04-30 22:51:20 -0500

    helper function to provide dates in the YYYY-MM-DD format..

--- Frontend/screens/BudgetManipulation/utils/formatters.js
+++ Frontend/screens/BudgetManipulation/utils/formatters.js
@@ -18,3 +18,12 @@ export const formatDate = (date) => {
     return (spent / budget) * 100
   }
   
+  export const toISOStringFormat = (date) => {
+    const d = new Date(date);
+    
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const day = String(d.getDate()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}`;
+  }
